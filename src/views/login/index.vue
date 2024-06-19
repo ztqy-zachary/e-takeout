3937451f5afb276f88652862f4f71b66d78ef6a1
ztqy <2947574050@qq.com>
2024-06-20 01:21:23 +0800

    first commit

---
+++ src/views/login/index.vue
@@ -0,0 +1,228 @@
+<template>
+  <div class="login">
+    <div class="login-box">
+      <img src="@/assets/login/login.png" alt="" />
+      <div class="login-form">
+        <el-form ref="loginForm" :model="loginForm" :rules="loginRules">
+          <div class="login-form-title">
+            <img
+              src="@/assets/login/logo_title.jpg"
+              style="width: 149px; height: 38px"
+              alt=""
+            />
+          </div>
+          <el-form-item prop="username">
+            <el-input
+              v-model="loginForm.username"
+              type="text"
+              auto-complete="off"
+              placeholder="usernam"
+              prefix-icon="iconfont icon-user"
+            />
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input
+              v-model="loginForm.password"
+              type="password"
+              placeholder="password"
+              prefix-icon="iconfont icon-lock"
+              @keyup.enter.native="handleLogin"
+            />
+          </el-form-item>
+          <el-form-item style="width: 100%">
+            <el-button
+              :loading="loading"
+              class="login-btn"
+              size="medium"
+              type="primary"
+              style="width: 100%"
+              @click.native.prevent="handleLogin"
+            >
+              <span v-if="!loading">登录</span>
+              <span v-else>登录中...</span>
+            </el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch } from 'vue-property-decorator'
+import { Route } from 'vue-router'
+import { Form as ElForm, Input } from 'element-ui'
+import { UserModule } from '@/store/modules/user'
+import { isValidUsername } from '@/utils/validate'
+
+@Component({
+  name: 'Login',
+})
+export default class extends Vue {
+  private validateUsername = (rule: any, value: string, callback: Function) => {
+    if (!value) {
+      callback(new Error('请输入用户名'))
+    } else {
+      callback()
+    }
+  }
+  private validatePassword = (rule: any, value: string, callback: Function) => {
+    if (value.length < 6) {
+      callback(new Error('密码必须在6位以上'))
+    } else {
+      callback()
+    }
+  }
+  private loginForm = {
+    username: 'admin',
+    password: '123456',
+  } as {
+    username: String
+    password: String
+  }
+
+  loginRules = {
+    username: [{ validator: this.validateUsername, trigger: 'blur' }],
+    password: [{ validator: this.validatePassword, trigger: 'blur' }],
+  }
+  private loading = false
+  private redirect?: string
+
+  @Watch('$route', { immediate: true })
+  private onRouteChange(route: Route) {}
+
+  // 登录
+  private handleLogin() {
+    (this.$refs.loginForm as ElForm).validate(async (valid: boolean) => {
+      if (valid) {
+        this.loading = true
+        await UserModule.Login(this.loginForm as any)
+          .then((res: any) => {
+            if (String(res.code) === '1') {
+              this.$router.push('/')
+            } else {
+              this.loading = false
+            }
+          })
+          .catch(() => {
+            this.loading = false
+          })
+      } else {
+        return false
+      }
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  background-image: url('../../assets/login/bgjpg.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.login-box {
+  width: 1500px;
+  height: 711.57px;
+  border-radius: 8px;
+  display: flex;
+  img {
+    width: 70%;
+    height: auto;
+  }
+}
+
+.title {
+  margin: 0px auto 10px auto;
+  text-align: left;
+  color: #707070;
+}
+
+.login-form {
+  background: #aac9ce;
+  width: 60%;
+  border-radius: 0px 8px 8px 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .el-form {
+    width: 214px;
+    height: 307px;
+  }
+  .el-form-item {
+    margin-bottom: 30px;
+  }
+  .el-form-item.is-error .el-input__inner {
+    border: 0 !important;
+    border-bottom: 1px solid #fd7065 !important;
+    background: #fff !important;
+  }
+  .input-icon {
+    height: 32px;
+    width: 18px;
+    margin-left: -2px;
+  }
+  .el-input__inner {
+    border: 0;
+    border-bottom: 1px solid #e9e9e8;
+    border-radius: 8px;
+    font-size: 30px;
+    font-weight: 400;
+    color: #333333;
+    height: 40px;
+    line-height: 40px;
+  }
+  .el-input__prefix {
+    left: 0;
+  }
+  .el-input--prefix .el-input__inner {
+    padding-left: 26px;
+  }
+  .el-input__inner::placeholder {
+    color: #aeb5c4;
+  }
+  .el-form-item--medium .el-form-item__content {
+    line-height: 55px;
+  }
+  .el-input--medium .el-input__icon {
+    line-height: 55px;
+  }
+}
+
+.login-btn {
+  border-radius: 17px;
+  padding: 11px 20px !important;
+  margin-top: 10px;
+  font-weight: 500;
+  font-size: 40px;
+  border: 0;
+  font-weight: 500;
+  color: #333333;
+  // background: #09a57a;
+  background-color: #2fb2c2;
+  &:hover,
+  &:focus {
+    background-color: green;
+    color: #ffffff;
+  }
+}
+.login-form-title {
+  height: 36px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 40px;
+  .title-label {
+    font-weight: 500;
+    font-size: 20px;
+    color: #333333;
+    margin-left: 10px;
+  }
+}
+</style>
